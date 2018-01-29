1620503c79ad32dfb92f4bc789d2cef859213d5b
GuangLi <guang.li@intelligentgroup.cn>
2018-01-29 09:51:45 +0800

    service api

--- dap-sdk/src/main/java/com/dmsoft/firefly/sdk/dai/dto/TemplateSettingDto.java
+++ dap-sdk/src/main/java/com/dmsoft/firefly/sdk/dai/dto/TemplateSettingDto.java
@@ -9,20 +9,10 @@ import java.util.LinkedHashMap;
  * Created by Guang.Li on 2018/1/29.
  */
 public class TemplateSettingDto {
-    private Long id ;
     private String name;
     private int decimalDigit;
     private TimePatternDto timePatternDto;
-//    private LinkedHashMap<String, LinkedHashMap<String, AlarmDataDto>> alarmDatas;
     private LinkedHashMap<String, SpecificationDataDto> specificationDatas;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
