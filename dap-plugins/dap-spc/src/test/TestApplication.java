9fe015e701872b45fac62ca0b15131b7ec75d20e
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-01-29 14:27:57 +0800

    add spc main view

---
+++ dap-plugins/dap-spc/src/test/TestApplication.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017. For Intelligent Group.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Ethan.Yang on 2018/1/29.
+ */
+public class TestApplication extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/spc.fxml"));
+        //root.getStylesheets().add("/main.css");
+
+        Scene scene = new Scene(root,1280,704);
+//        scene.getStylesheets().add(getClass().getClassLoader().getResource("css/main.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
