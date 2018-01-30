5b98de2ec0a2df2d4b4440feb432f6b212db1ac6
peter.li <peter.li@intelligentgroup.cn>
2018-01-30 17:27:07 +0800

    Merge branch 'develop' of http://iagit.cygia.com/dmsw_spc/firefly into develop
    * 'develop' of http://iagit.cygia.com/dmsw_spc/firefly:
      add spc main view
      add start method
      add start method
    
    # Conflicts:
    #	dap-core/src/main/java/com/dmsoft/firefly/core/DAPApplication.java
    #	dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/SpcPlugin.java

--- dap-sdk/src/main/java/com/dmsoft/firefly/sdk/plugin/Plugin.java
+++ dap-sdk/src/main/java/com/dmsoft/firefly/sdk/plugin/Plugin.java
@@ -25,7 +25,7 @@ public abstract class Plugin {
     /**
      * start method will be called after all initialize method is done.
      */
-    public abstract void start() throws Exception;
+    public abstract void start();
 
     /**
      * destroy method will be called when this plugin is disable.
