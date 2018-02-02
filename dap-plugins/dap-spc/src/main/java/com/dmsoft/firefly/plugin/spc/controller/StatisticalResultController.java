126051e4886f472a0fc26542059e5d5bd9c3cc66
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-02-02 15:26:00 +0800

    add spc

---
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/controller/StatisticalResultController.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017. For Intelligent Group.
+ */
+package com.dmsoft.firefly.plugin.spc.controller;
+
+/**
+ * Created by Ethan.Yang on 2018/2/2.
+ */
+public class StatisticalResultController {
+}
