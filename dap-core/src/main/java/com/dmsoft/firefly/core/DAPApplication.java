96b2a5b40911ade76ae58fe80182624cf02af25f
Can Guan <can.guan@cygia.com>
2018-02-05 11:27:46 +0800

    update run method

--- dap-core/src/main/java/com/dmsoft/firefly/core/DAPApplication.java
+++ dap-core/src/main/java/com/dmsoft/firefly/core/DAPApplication.java
@@ -61,12 +61,12 @@ public class DAPApplication {
             List<PluginInfo> scannedPlugins = PluginScanner.scanPluginByPath(pluginFolderPath);
             RuntimeContext.getBean(PluginContext.class).installPlugin(scannedPlugins);
             RuntimeContext.getBean(PluginContext.class).enablePlugin(activePlugins);
-            DAPClassLoader loader = RuntimeContext.getBean(PluginContext.class).getDAPClassLoader("com.dmsoft.dap.SpcPlugin");
-            Class c = loader.loadClass("com.dmsoft.firefly.plugin.spc.SpcService");
-            System.out.println(c);
-            PluginProxyMethod method = RuntimeContext.getBean(PluginProxyMethodFactory.class).proxyMethod("com.dmsoft.dap.SpcPlugin", "com.dmsoft.firefly.plugin.spc.SpcService", "say");
-            method.doSomething(null, "AA");
-            System.out.println("SADF");
+//            DAPClassLoader loader = RuntimeContext.getBean(PluginContext.class).getDAPClassLoader("com.dmsoft.dap.SpcPlugin");
+//            Class c = loader.loadClass("com.dmsoft.firefly.plugin.spc.SpcService");
+//            System.out.println(c);
+//            PluginProxyMethod method = RuntimeContext.getBean(PluginProxyMethodFactory.class).proxyMethod("com.dmsoft.dap.SpcPlugin", "com.dmsoft.firefly.plugin.spc.SpcService", "say");
+//            method.doSomething(null, "AA");
+//            System.out.println("SADF");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -78,7 +78,7 @@ public class DAPApplication {
                 e.printStackTrace();
             }
         }
-        pluginInfoContextImpl.startPlugin("com.dmsoft.dap.SpcPlugin");
+        pluginInfoContextImpl.startPlugin(activePlugins);
         return pluginInfoContextImpl;
     }
 
