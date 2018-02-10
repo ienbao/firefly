63fac7ed127afa469cd688656ae487fde4903b9a
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-02-10 09:31:17 +0800

    add spc controller

--- dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/dto/SpcViewDataDto.java
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/dto/SpcViewDataDto.java
@@ -5,8 +5,29 @@ package com.dmsoft.firefly.plugin.spc.dto;
 
 import com.dmsoft.bamboo.common.dto.AbstractValueObject;
 
+import java.util.Map;
+
 /**
  * Created by Ethan.Yang on 2018/2/6.
  */
 public class SpcViewDataDto extends AbstractValueObject {
+
+    private String lineKey;
+    private Map<String,Object> testData;
+
+    public String getLineKey() {
+        return lineKey;
+    }
+
+    public void setLineKey(String lineKey) {
+        this.lineKey = lineKey;
+    }
+
+    public Map<String, Object> getTestData() {
+        return testData;
+    }
+
+    public void setTestData(Map<String, Object> testData) {
+        this.testData = testData;
+    }
 }
--- dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/service/impl/SpcService.java
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/service/impl/SpcService.java
@@ -6,44 +6,57 @@ package com.dmsoft.firefly.plugin.spc.service.impl;
 import com.dmsoft.firefly.plugin.spc.dto.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Ethan.Yang on 2018/2/5.
  */
 public interface SpcService {
-    /**
-     * To get spc statistical result.
-     *
-     * @param searchConditionDtoList list search condition
-     * @param spcSearchConfigDto  search config
+
+     /**
+     *To get spc statistical result.
+     * @param testItemNames
+     * @param conditions
+     * @param spcSearchConfigDto
      * @return list of spc statistical result
      */
-    List<SpcStatisticalResultDto> findStatisticalResult(List<SearchConditionDto> searchConditionDtoList, SpcSearchConfigDto spcSearchConfigDto);
+    List<SpcStatisticalResultDto> findStatisticalResult(List<String> testItemNames,List<String> conditions,SpcSearchConfigDto spcSearchConfigDto);
+
+    /**
+     * To find chart and view data.
+     * @param searchConditionDtoList
+     * @param spcSearchConfigDto search data
+     * @return the detail result
+     */
+    SpcDetailResultDto findChartDataAndViewData(List<SearchConditionDto> searchConditionDtoList,SpcSearchConfigDto spcSearchConfigDto);
 
     /**
      * To refresh spc statistical result.
-     * @param spcAnalysisDataDto
+     * @param searchConditionDtoList
      * @param spcSearchConfigDto
      * @return list of spc statistical result
      */
-    List<SpcStatisticalResultDto> refreshStatisticalResult(List<SpcAnalysisDataDto> spcAnalysisDataDto, SpcSearchConfigDto spcSearchConfigDto);
-
+    List<SpcStatisticalResultDto> refreshStatisticalResult(List<SearchConditionDto> searchConditionDtoList,SpcSearchConfigDto spcSearchConfigDto);
 
     /**
-     * To find spc chart result.
-     * @param spcAnalysisDataDto
+     * To refresh all analysis result.
+     * @param searchConditionDtoList
      * @param spcSearchConfigDto
-     * @return list of the chart result
+     * @param includeLineNo the lineNo of file
+     * @return
      */
-    List<SpcChartResultDto> findChartResult(List<SpcAnalysisDataDto> spcAnalysisDataDto, SpcSearchConfigDto spcSearchConfigDto);
+    SpcAnalysisResultDto refreshAllAnalysisResult(List<SearchConditionDto> searchConditionDtoList,SpcSearchConfigDto spcSearchConfigDto,Map<String,List<Long>> includeLineNo);
 
     /**
      * To find view data result.
      * @param searchConditionDtoList
      * @param spcSearchConfigDto
+     * @param testItems
      * @return the spc view data
      */
-    SpcViewDataDto findViewData(List<SearchConditionDto> searchConditionDtoList, SpcSearchConfigDto spcSearchConfigDto);
+    SpcViewDataDto updateViewData(List<SearchConditionDto> searchConditionDtoList, SpcSearchConfigDto spcSearchConfigDto, List<String> testItems);
+
+
 
 
 
