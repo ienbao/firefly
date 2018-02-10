ea9f32c43de9642a4d0e271b02fd02517d415f10
julia.zhou <li.zhou@intelligentgroup.cn>
2018-02-10 15:39:18 +0800

    add pane cache improved controller

---
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/utils/ViewResource.java
@@ -0,0 +1,29 @@
+package com.dmsoft.firefly.plugin.spc.utils;
+
+public final class ViewResource {
+
+    public final static String PRE_VIEW = "view/";
+
+    public final static String  SPC_VIEW_ID = "spc";
+    public final static String  SPC_VIEW_RES = PRE_VIEW + "spc.fxml";
+
+    public final static String _SPC_ITEM_VIEW_ID = "spc_item";
+    public final static String  SPC_ITEM__VIEW_RES = PRE_VIEW + "spc_item.fxml";
+
+    public final static String  SPC_STATISTICAL_VIEW_ID = "spc_statistical";
+    public final static String  SPC_STATISTICAL_VIEW_RES = PRE_VIEW + "statistical_result.fxml";
+
+    public final static String  SPC_VIEW_DATA_VIEW_ID = "spc_view_data";
+    public final static String  SPC_VIEW_DATA_VIEW_RES = PRE_VIEW + "view_data.fxml";
+
+    public final static String  SPC_CHART_VIEW_ID = "spc_chart";
+    public final static String  SPC_CHART_VIEW_RES = PRE_VIEW + "chart_result.fxml";
+
+
+    public final static String  SPC_CHOOSE_STATISTICAL_VIEW_ID = "spc_choose_statistical";
+    public final static String  SPC_CHOOSE_STATISTICAL_VIEW_RES = PRE_VIEW + "choose_statistical_result.fxml";
+
+
+    public final static String  SPC_QUICK_SEARCH_VIEW_ID = "spc_quick_search";
+    public final static String  SPC_QUICK_SEARCH_VIEW_RES = PRE_VIEW + "quick_search.fxml";
+}
