e5a1b16706a64e746cfa9599459fcdbbe461be4b
peter.li <peter.li@intelligentgroup.cn>
2018-02-11 10:41:46 +0800

    return the stage created by factory.

--- dap-sdk/src/main/java/com/dmsoft/firefly/sdk/ui/window/WindowFactory.java
+++ dap-sdk/src/main/java/com/dmsoft/firefly/sdk/ui/window/WindowFactory.java
@@ -2,69 +2,62 @@ package com.dmsoft.firefly.sdk.ui.window;
 
 import com.dmsoft.firefly.sdk.utils.StageMap;
 import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class WindowFactory {
 
-    public static void createFullWindow(String name, String title, Pane bodyPane, String style) {
+    public static Stage createFullWindow(String name, String title, Pane bodyPane, String style) {
         WindowPane windowPane = new WindowPane(title, bodyPane);
-
         if (StageMap.loadStage(name, windowPane, false, style, StageStyle.TRANSPARENT)) {
-//            Stage stage = StageMap.getStage(name);
+            Stage stage = StageMap.getStage(name);
             windowPane.setStage(StageMap.getStage(name));
             windowPane.init();
+            return stage;
         }
 
+        return null;
     }
 
-    public static void createFullWindow(String name, Pane title, Pane bodyPane, String style) {
+    public static Stage createFullWindow(String name, Pane title, Pane bodyPane, String style) {
         WindowPane windowPane = new WindowPane(title, bodyPane);
 
         if (StageMap.loadStage(name, windowPane, false, style, StageStyle.TRANSPARENT)) {
             Stage stage = StageMap.getStage(name);
             windowPane.setStage(stage);
             windowPane.init();
+            return stage;
         }
 
+        return null;
     }
 
-
-    public static void createFullWindowAsModel(String name, String title, Pane bodyPane, String style) {
+    public static Stage createFullWindowAsModel(String name, String title, Pane bodyPane, String style) {
         WindowPane windowPane = new WindowPane(title, bodyPane);
 
         if (StageMap.loadStage(name, windowPane, true, style, StageStyle.TRANSPARENT)) {
             Stage stage = StageMap.getStage(name);
             windowPane.setStage(stage);
             windowPane.init();
+            return stage;
         }
 
+        return null;
     }
 
-    public static void createFullWindowAsModel(String name, Pane title, Pane bodyPane, String style) {
+    public static Stage createFullWindowAsModel(String name, Pane title, Pane bodyPane, String style) {
         WindowPane windowPane = new WindowPane(title, bodyPane);
         if (StageMap.loadStage(name, windowPane, true, style, StageStyle.TRANSPARENT)) {
             Stage stage = StageMap.getStage(name);
             windowPane.setStage(stage);
             windowPane.init();
+            return stage;
         }
 
+        return null;
     }
 
-    public static void createSimpleWindowAsModel(String name, Pane title, Pane bodyPane, String style) {
-        WindowPane windowPane = new WindowPane(title, bodyPane);
-        windowPane.setWindowsModel(WindowPane.WINDOW_MODEL_X);
-
-        if (StageMap.loadStage(name, windowPane, true, style, StageStyle.TRANSPARENT)) {
-            Stage stage = StageMap.getStage(name);
-            windowPane.setStage(stage);
-            windowPane.init();
-        }
-
-    }
-
-    public static void createSimpleWindowAsModel(String name, String title, Pane bodyPane, String style) {
+    public static Stage createSimpleWindowAsModel(String name, Pane title, Pane bodyPane, String style) {
         WindowPane windowPane = new WindowPane(title, bodyPane);
         windowPane.setWindowsModel(WindowPane.WINDOW_MODEL_X);
 
@@ -72,7 +65,23 @@ public class WindowFactory {
             Stage stage = StageMap.getStage(name);
             windowPane.setStage(stage);
             windowPane.init();
+            return stage;
         }
 
+        return null;
+    }
+
+    public static Stage createSimpleWindowAsModel(String name, String title, Pane bodyPane, String style) {
+        WindowPane windowPane = new WindowPane(title, bodyPane);
+        windowPane.setWindowsModel(WindowPane.WINDOW_MODEL_X);
+
+        if (StageMap.loadStage(name, windowPane, true, style, StageStyle.TRANSPARENT)) {
+            Stage stage = StageMap.getStage(name);
+            windowPane.setStage(stage);
+            windowPane.init();
+            return stage;
+        }
+
+        return null;
     }
 }
