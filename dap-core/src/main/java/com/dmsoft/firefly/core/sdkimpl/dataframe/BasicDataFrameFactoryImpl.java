632cce09b1fa40a340268afbcbc00f556f83443c
Can Guan <can.guan@cygia.com>
2018-02-27 11:19:01 +0800

    add data frame impl

---
+++ dap-core/src/main/java/com/dmsoft/firefly/core/sdkimpl/dataframe/BasicDataFrameFactoryImpl.java
@@ -0,0 +1,48 @@
+package com.dmsoft.firefly.core.sdkimpl.dataframe;
+
+import com.dmsoft.firefly.sdk.dai.dto.RowDataDto;
+import com.dmsoft.firefly.sdk.dai.dto.TestItemWithTypeDto;
+import com.dmsoft.firefly.sdk.dataframe.DataColumn;
+import com.dmsoft.firefly.sdk.dataframe.DataFrame;
+import com.dmsoft.firefly.sdk.dataframe.DataFrameFactory;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * basic data frame factory impl for data frame factory
+ *
+ * @author Can Guan
+ */
+public class BasicDataFrameFactoryImpl implements DataFrameFactory {
+    @Override
+    public DataFrame createDataFrame(List<TestItemWithTypeDto> testItemWithTypeDtoList, List<RowDataDto> rowDataDtoList) {
+        return new BasicDataFrame(testItemWithTypeDtoList, rowDataDtoList);
+    }
+
+    @Override
+    public List<DataColumn> createDataColumn(List<TestItemWithTypeDto> testItemWithTypeDtoList, List<RowDataDto> rowDataDtoList) {
+        if (testItemWithTypeDtoList == null || rowDataDtoList == null || testItemWithTypeDtoList.size() != rowDataDtoList.size()) {
+            return null;
+        }
+        List<DataColumn> result = Lists.newArrayList();
+        for (int i = 0; i < testItemWithTypeDtoList.size(); i++) {
+            TestItemWithTypeDto testItemWithTypeDto = testItemWithTypeDtoList.get(i);
+            if (testItemWithTypeDto.getTestItemName() != null) {
+                List<String> rowKeyList = Lists.newArrayList();
+                List<String> valueList = Lists.newArrayList();
+                List<Boolean> inUsedList = Lists.newArrayList();
+                for (int j = 0; j < testItemWithTypeDtoList.size(); j++) {
+                    if (rowDataDtoList.get(i).getRowKey() != null) {
+                        rowKeyList.add(rowDataDtoList.get(i).getRowKey());
+                        valueList.add(rowDataDtoList.get(i).getData().get(testItemWithTypeDto.getTestItemName()));
+                        inUsedList.add(rowDataDtoList.get(i).getInUsed());
+                    }
+                }
+                DataColumn dataColumn = new BasicDataColumn(testItemWithTypeDto, rowKeyList, valueList, inUsedList);
+                result.add(dataColumn);
+            }
+        }
+        return result;
+    }
+}
