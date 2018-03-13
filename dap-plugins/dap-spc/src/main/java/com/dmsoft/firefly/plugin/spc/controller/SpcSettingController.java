3cf6d3f9b1c6c1dd669bf73af046531e391f015e
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-03-13 18:37:23 +0800

    add spc setting

---
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/controller/SpcSettingController.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017. For Intelligent Group.
+ */
+package com.dmsoft.firefly.plugin.spc.controller;
+
+/**
+ * Created by Ethan.Yang on 2018/3/13.
+ */
+public class SpcSettingController {
+}
