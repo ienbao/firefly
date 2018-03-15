d3fa388ef8dd2badb6ee8241b33f81215350d4d7
Can Guan <can.guan@cygia.com>
2018-03-15 09:41:31 +0800

    add semaphore / add error code for analysis service

--- dap-plugins/dap-grr/src/main/java/com/dmsoft/firefly/plugin/grr/utils/GrrExceptionCode.java
+++ dap-plugins/dap-grr/src/main/java/com/dmsoft/firefly/plugin/grr/utils/GrrExceptionCode.java
@@ -5,6 +5,9 @@ public class GrrExceptionCode {
     //invalid spc parameter!
     public static final int ERR_11001 = 11001;
 
+    //analyze unhandle error!
+    public static final int ERR_12001 = 12001;
+
     //system error
     public static final int ERR_20001 = 20001;
 }
