3b4f117173f8d54610400183a81be6572b9d739c
GuangLi <guang.li@intelligentgroup.cn>
2018-03-16 14:24:01 +0800

    mongo install

--- dap-gui/src/main/resources/scripts/installmongo.bat
+++ dap-gui/src/main/resources/scripts/installmongo.bat
@@ -4,9 +4,5 @@ set MONGO=%~dp0mongodb
 
 IF EXIST "%MONGO%" (
 cd %MONGO%\bin
-net stop MongoDB
-mongod --remove MongoDB
-mongod --repair --dbpath %MONGO%\data\db
-mongod --config %MONGO%\data\log\mongodb.log --install --serviceName "MongoDB"
-net start MongoDB
+mongod --dbpath=%MONGO%\data\db --logpath=%MONGO%\data\log\mongo.log
 )
