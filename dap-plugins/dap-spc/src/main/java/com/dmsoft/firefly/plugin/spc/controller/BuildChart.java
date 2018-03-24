a07b593bdb36f32e985f4a72488571b70f561957
LiGuang <guang.li@intelligentgroup.cn>
2018-03-24 09:58:07 +0800

    export

--- dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/controller/BuildChart.java
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/controller/BuildChart.java
@@ -6,7 +6,6 @@ import com.dmsoft.firefly.plugin.spc.charts.NDChart;
 import com.dmsoft.firefly.plugin.spc.charts.data.BoxPlotChartData;
 import com.dmsoft.firefly.plugin.spc.charts.data.ControlChartData;
 import com.dmsoft.firefly.plugin.spc.charts.data.NDBarChartData;
-import com.dmsoft.firefly.plugin.spc.charts.data.basic.*;
 import com.dmsoft.firefly.plugin.spc.charts.utils.MathUtils;
 import com.dmsoft.firefly.plugin.spc.dto.SpcChartDto;
 import com.dmsoft.firefly.plugin.spc.dto.analysis.SpcChartResultDto;
@@ -17,7 +16,6 @@ import com.dmsoft.firefly.sdk.utils.ColorUtils;
 import com.dmsoft.firefly.sdk.utils.DAPStringUtils;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -39,8 +37,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by GuangLi on 2018/3/14.
@@ -247,10 +243,13 @@ public class BuildChart {
             yLower[i] = (Double) ndChartData.get(i).getYLowerBound();
             yUpper[i] = (Double) ndChartData.get(i).getYUpperBound();
         }
-        double xMax = MathUtils.getMax(xUpper);
-        double xMin = MathUtils.getMin(xLower);
-        double yMax = MathUtils.getMax(yUpper);
-        double yMin = MathUtils.getMin(yLower);
+        Double xMax = MathUtils.getMax(xUpper);
+        Double xMin = MathUtils.getMin(xLower);
+        Double yMax = MathUtils.getMax(yUpper);
+        Double yMin = MathUtils.getMin(yLower);
+        if (xMax == null || xMin == null || yMax == null || yMin == null) {
+            return;
+        }
         NumberAxis xAxis = (NumberAxis) chart.getXAxis();
         NumberAxis yAxis = (NumberAxis) chart.getYAxis();
         double yReserve = (yMax - yMin) * UIConstant.FACTOR;
@@ -303,10 +302,13 @@ public class BuildChart {
             yLower[i] = (Double) controlChartData.get(i).getYLowerBound();
             yUpper[i] = (Double) controlChartData.get(i).getYUpperBound();
         }
-        double xMax = MathUtils.getMax(xUpper);
-        double xMin = MathUtils.getMin(xLower);
-        double yMax = MathUtils.getMax(yUpper);
-        double yMin = MathUtils.getMin(yLower);
+        Double xMax = MathUtils.getMax(xUpper);
+        Double xMin = MathUtils.getMin(xLower);
+        Double yMax = MathUtils.getMax(yUpper);
+        Double yMin = MathUtils.getMin(yLower);
+        if (xMax == null || xMin == null || yMax == null || yMin == null) {
+            return;
+        }
         NumberAxis xAxis = (NumberAxis) chart.getXAxis();
         NumberAxis yAxis = (NumberAxis) chart.getYAxis();
         double yReserve = (yMax - yMin) * UIConstant.FACTOR;
@@ -339,9 +341,9 @@ public class BuildChart {
         Double xMin = MathUtils.getMin(xLower);
         Double yMax = MathUtils.getMax(yUpper);
         Double yMin = MathUtils.getMin(yLower);
-//        if (xMax == null || xMin == null || yMax == null || yMin == null) {
-//            return;
-//        }
+        if (xMax == null || xMin == null || yMax == null || yMin == null) {
+            return;
+        }
         NumberAxis xAxis = (NumberAxis) chart.getXAxis();
         NumberAxis yAxis = (NumberAxis) chart.getYAxis();
         double yReserve = (yMax - yMin) * UIConstant.FACTOR;
