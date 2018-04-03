6e078d89ad5e2090f0ec0f9819267d391bdcc23f
Can <can.guan@cygia.com>
2018-04-03 09:31:52 +0800

    fix cls in ndc

--- dap-plugins/dap-grr/src/main/java/com/dmsoft/firefly/plugin/grr/controller/GrrItemController.java
+++ dap-plugins/dap-grr/src/main/java/com/dmsoft/firefly/plugin/grr/controller/GrrItemController.java
@@ -300,7 +300,7 @@ public class GrrItemController implements Initializable {
         }
     }
 
-    private void updateAppraiserListViewDatas(Set<String> selectedAppraisers,  String appraiser) {
+    private void updateAppraiserListViewDatas(Set<String> selectedAppraisers, String appraiser) {
         appraiserListView.getItems().clear();
         appraiserList.clear();
         if (isContainValue(appraiser, appraiserCombox)) {
@@ -699,6 +699,7 @@ public class GrrItemController implements Initializable {
         if (checkSubmitParam(selectedItemDto.size())) {
             JobContext context = RuntimeContext.getBean(JobFactory.class).createJobContext();
             WindowProgressTipController windowProgressTipController = WindowMessageFactory.createWindowProgressTip();
+            windowProgressTipController.setAutoHide(false);
             List<String> projectNameList = envService.findActivatedProjectName();
             List<TestItemWithTypeDto> testItemWithTypeDtoList = this.buildSelectTestItemWithTypeData(selectedItemDto);
             context.put(ParamKeys.PROJECT_NAME_LIST, projectNameList);
@@ -1037,8 +1038,8 @@ public class GrrItemController implements Initializable {
                 }
             } else {
                 RuntimeContext.getBean(IMessageManager.class).showWarnMsg(
-                    GrrFxmlAndLanguageUtils.getString(UIConstant.UI_MESSAGE_TIP_WARNING_TITLE),
-                    GrrFxmlAndLanguageUtils.getString("IMPORT_EXCEPTION"));
+                        GrrFxmlAndLanguageUtils.getString(UIConstant.UI_MESSAGE_TIP_WARNING_TITLE),
+                        GrrFxmlAndLanguageUtils.getString("IMPORT_EXCEPTION"));
             }
 
         }
