956201fd239144542ac9da3b11a016a783bf963e
Can <can.guan@cygia.com>
2018-04-12 09:22:35 +0800

    fix bug#1290

--- dap-gui/src/main/resources/scripts/installmongo.bat
+++ dap-gui/src/main/resources/scripts/installmongo.bat
@@ -3,6 +3,6 @@
 set MONGO=%~dp0mongodb
 
 IF EXIST "%MONGO%" (
-cd %MONGO%\bin
-mongod --dbpath=%MONGO%\data\db --logpath=%MONGO%\data\log\mongo.log
+cd "%MONGO%\bin"
+mongod --dbpath="%MONGO%\data\db" --logpath="%MONGO%\data\log\mongo.log"
 )
