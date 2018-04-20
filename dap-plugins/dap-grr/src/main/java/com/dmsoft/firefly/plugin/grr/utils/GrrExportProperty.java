441406b070d57c0bbfc5b4572e5375ef89119cde
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-04-20 16:22:15 +0800

    fix export

---
+++ dap-plugins/dap-grr/src/main/java/com/dmsoft/firefly/plugin/grr/utils/GrrExportProperty.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2017. For Intelligent Group.
+ */
+package com.dmsoft.firefly.plugin.grr.utils;
+
+/**
+ * Created by Ethan.Yang on 2018/4/20.
+ */
+public class GrrExportProperty {
+    public static final int EXPORT_EXCEL_TEST_ITEM_GROUP_SIZE = 100;
+    public static final int EXPORT_EXCEL_SHEET_SIZE = 200;
+}
