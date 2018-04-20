7e10bbd6f201064523450c8579abc448e63932fd
Ethan Yang <xiong.yang@intelligentgroup.cn>
2018-04-20 09:28:09 +0800

    add exception code il8n

--- dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/utils/SpcExceptionCode.java
+++ dap-plugins/dap-spc/src/main/java/com/dmsoft/firefly/plugin/spc/utils/SpcExceptionCode.java
@@ -2,11 +2,11 @@ package com.dmsoft.firefly.plugin.spc.utils;
 
 public class SpcExceptionCode {
     //SPC
-    //service unhandle error!
+    //service unhandled error!
     public static final int ERR_11001 = 11001;
     //invalid spc parameter!
     public static final int ERR_11002 = 11002;
-    //analyze unhandle error!
+    //analyze unhandled error!
     public static final int ERR_11005 = 11005;
 
     //system error
