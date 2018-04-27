922cdea3d1fef3ea69a398e98e6b695178dfabec
Baofeng Zhu <baofeng.zhu@cygia.com>
2018-04-27 09:04:14 +0800

    commit the echo output

--- dap-gui/src/main/resources/scripts/installmongo.bat
+++ dap-gui/src/main/resources/scripts/installmongo.bat
@@ -11,26 +11,26 @@ set ID=
 
 :start
    for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":27018"') do (
-     echo exit starting mongodb
+     rem echo exit starting mongodb
      set flag=1
   )
-  echo if exit starting mongodb: %flag%
+  rem echo if exit starting mongodb: %flag%
   IF EXIST "%MONGO%" ( IF %flag%==0 (
 
      IF NOT EXIST "%DB%" (
-          ECHO "%DB%" is not exist.
+         rem  ECHO "%DB%" is not exist.
           md "%~dp0%DB%"
-          ECHO New "%DB%" success.
+         rem  ECHO New "%DB%" success.
        )
        IF NOT EXIST "%LOG%" (
-         ECHO "%LOG%" is not exist.
+        rem  ECHO "%LOG%" is not exist.
         md "%~dp0%LOG%"
-        ECHO New "%LOG%" success.
+        rem ECHO New "%LOG%" success.
       )
 
 
        cd "%MONGO%\bin"
-       echo start mongod.exe
+      rem echo start mongod.exe
         mongod --dbpath="%~dp0%DB%" --port=27018 --logpath="%~dp0%LOG%\mongo.log" --wiredTigerCacheSizeGB 4
      ))
     exit
