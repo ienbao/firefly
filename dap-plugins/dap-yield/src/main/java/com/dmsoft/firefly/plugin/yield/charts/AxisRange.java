c30f32de48fdf5dd49bc5d4e472a3fa61635e3b0
yuanxingtian <yuanxing.tian@cygia.com>
2018-10-18 18:08:09 +0800

    add YieldNDChart

---
+++ dap-plugins/dap-yield/src/main/java/com/dmsoft/firefly/plugin/yield/charts/AxisRange.java
@@ -0,0 +1,31 @@
+package com.dmsoft.firefly.plugin.yield.charts;
+
+public interface AxisRange <X extends Number, Y extends Number>{
+//    /**
+//     * Get x lower value
+//     *
+//     * @return x lower value
+//     */
+//    X getXLowerBound();
+//
+//    /**
+//     * Get x upper value
+//     *
+//     * @return x upper value
+//     */
+//    X getXUpperBound();
+//
+//    /**
+//     * Get y lower value
+//     *
+//     * @return y lower value
+//     */
+//    Y getYLowerBound();
+//
+//    /**
+//     * Get y upper value
+//     *
+//     * @return y upper value
+//     */
+//    Y getYUpperBound();
+}
---
+++ dap-plugins/dap-yield/src/main/java/com/dmsoft/firefly/plugin/yield/charts/data/basic/IXYChartData.java
@@ -0,0 +1,78 @@
+package com.dmsoft.firefly.plugin.yield.charts.data.basic;
+
+import com.dmsoft.firefly.plugin.spc.charts.data.basic.PointRule;
+import com.dmsoft.firefly.plugin.spc.charts.data.basic.PointStyle;
+import javafx.scene.paint.Color;
+
+import java.util.function.Function;
+
+/**
+ * Created by Tommy on 2018/10/18.
+ */
+
+/**
+ * XY chart data
+ *
+ * @param <X> x data class
+ * @param <Y> y data class
+ */
+public interface IXYChartData<X, Y> {
+
+    /**
+     * Get point count
+     *
+     * @return point count
+     */
+    int getLen();
+
+    /**
+     * Get x value by index
+     *
+     * @param index index
+     * @return x value
+     */
+    X getXValueByIndex(int index);
+
+    /**
+     * Get y value by index
+     *
+     * @param index index
+     * @return y value
+     */
+    Y getYValueByIndex(int index);
+
+    /**
+     * Get extra value by index
+     *
+     * @param index index
+     * @return extra value
+     */
+    Object getExtraValueByIndex(int index);
+
+    /**
+     * Get current series color
+     *
+     * @return series color
+     */
+    default Color getColor() {
+        return null;
+    }
+
+    /**
+     * Get series name
+     *
+     * @return series name
+     */
+    default String getSeriesName() {
+        return "";
+    }
+
+    /**
+     * Get point rule function
+     *
+     * @return point rule function
+     */
+    default Function<PointRule, PointStyle> getPointFunction() {
+        return null;
+    }
+}
