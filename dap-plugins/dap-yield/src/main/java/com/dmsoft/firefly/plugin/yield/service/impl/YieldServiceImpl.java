c5ba4c50bc4a8ecc59dabb6cffb338ff665591fb
Tod <tod.dylan@cygia.com>
2018-10-27 11:04:09 +0800

    修改测试项为空bug

--- dap-plugins/dap-yield/src/main/java/com/dmsoft/firefly/plugin/yield/service/impl/YieldServiceImpl.java
+++ dap-plugins/dap-yield/src/main/java/com/dmsoft/firefly/plugin/yield/service/impl/YieldServiceImpl.java
@@ -112,6 +112,11 @@ public class YieldServiceImpl implements YieldService {
                             ignoreTestItemValueList.add(ignoreTestItemValue);
                         }
                     }
+                } else {
+                    IgnoreTestItemValue ignoreTestItemValue = new IgnoreTestItemValue();
+                    ignoreTestItemValue.setRowKey(noNullProductSearchRowKeys.get(i));
+                    ignoreTestItemValue.setSearchConditionDto(searchConditions.get(j));
+                    ignoreTestItemValueList.add(ignoreTestItemValue);
                 }
             }
             if (count <= searchConditions.size() - 1 && count > 0) {
