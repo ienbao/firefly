205855d4c7d8eb004317a0ed87eb80d00b192302
yuanxingtian <yuanxing.tian@cygia.com>
2018-11-13 16:28:16 +0800

    优化：TabPaneSkin and css

--- dap-gui/src/main/java/TestCssDemo2.java
+++ dap-gui/src/main/java/TestCssDemo2.java
@@ -4,6 +4,7 @@ import com.dmsoft.firefly.sdk.RuntimeContext;
 import com.dmsoft.firefly.sdk.ui.PluginUIContext;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -16,47 +17,46 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TestCssDemo2 extends Application {
     private GridPane gridPane;
     private StackPane contentStackPane;
     private Map<String, TabPane> tabPaneMap = new LinkedHashMap<>();
-    private TabPane tabPane;
-    private PluginUIContext pc = RuntimeContext.getBean(PluginUIContext.class);
 
     @Override
     public void start(Stage stage) throws IOException {
         //通过fxml的UI布局文件加载实际的界面内容
-//      Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/test_demo2.fxml"));
-//      root.getStylesheets().add("css/test_demo2.css");
-
         gridPane = initTabPane();
-
+        gridPane.setMinWidth(1400);
         Scene scene = new Scene(gridPane);
+
+        scene.getStylesheets().add("css/redfall/testdemo2.css");
         stage.setScene(scene);
         stage.show();
     }
-
     private GridPane initTabPane() {
         String name = "name_1";
-
         GridPane gridPane = new GridPane();
-        HBox hBox = new HBox();
+        StackPane contentStackPane = new StackPane();
+        contentStackPane.setMaxWidth(1400);
+        contentStackPane.setMinWidth(1400);
         TabPane tabPane = new TabPane();
+        tabPane.setTabMinWidth(70);
         Tab tab = null;
-
         tabPane.setId(name);
-        for (int i = 0; i < 10; i++) {
+        //for (int i = 0; i < 10; i++) {
             tab = new Tab();
-            String [] namee = name.toString().split("_");
-            tab.setText(namee[0]+"_"+(i+1));
+            String [] namee = name.split("_");
+            tab.setText(namee[0]+"_"+(1));
             tabPane.getTabs().add(tab);
             tabPaneMap.put(name,tabPane);
-        }
-        hBox.getChildren().add(tabPane);
+       // }
+        contentStackPane.getChildren().add(tabPane);
         tabPane.setStyle("-fx-skin: 'com.dmsoft.firefly.gui.component.TabPaneSkinDemo'");
-        gridPane.add(hBox, 0, 0);
-
+        gridPane.add(contentStackPane, 0, 0);
+        System.out.println(tabPane.getStyleClass());
+        System.out.println(tab.getStyleClass());
         TabUtils.tabSelectedListener(tab, tabPane);
         TabUtils.disableCloseTab(tabPane);
         return gridPane;
